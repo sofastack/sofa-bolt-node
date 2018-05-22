33003275e1e5da852e0294773db731c9f3b28282
gxcsoccer <gxcsoccer@126.com>
2018-05-22 16:32:11 +0800

    chore: init commit

---
+++ .autod.conf.js
@@ -0,0 +1,19 @@
+'ues strict';
+
+module.exports = {
+  write: true,
+  prefix: '^',
+  devprefix: '^',
+  exclude: [
+    'test/fixtures',
+  ],
+  devdep: [
+    'autod',
+    'egg-bin',
+    'egg-ci',
+    'eslint',
+    'eslint-config-egg',
+  ],
+  keep: [],
+  semver: [],
+};
